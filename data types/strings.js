b34d53f91703bedec11ad6713a9184edf6b4f88f
Egor Mizerov <mizerov-2003@mail.ru>
2020-12-12 15:17:24 +0500

    add strings.js

---
+++ data types/strings.js
@@ -0,0 +1,16 @@
+/* Спецсимволы
+\n - перевод строки
+\"\' - ковычки
+\\ - обратный слеш
+\t - табуляция
+\uXX - Unicode
+*/
+
+let str = "Hello, World!"
+str.length              // 13 | Длина строки
+str[0]                  // H  | Доступ к символам
+str.indexOf("World")    // 7  | Поиск позиции строки
+str.includes("Hello")   // true | Существование строки
+str.slice(7, 12)        // World | Срез строки
+str.toUpperCase         // "HELLO, WORLD!"
+str.toLowerCase         // "hello, world!"
