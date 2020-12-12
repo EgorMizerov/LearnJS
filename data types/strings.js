acf1f475aa5563b8b7c368ecdd9d5497053af4d6
Egor Mizerov <mizerov-2003@mail.ru>
2020-12-12 15:20:25 +0500

    edit strings.js

--- data types/strings.js
+++ data types/strings.js
@@ -6,11 +6,11 @@
 \uXX - Unicode
 */
 
-let str = "Hello, World!"
-str.length              // 13 | Длина строки
-str[0]                  // H  | Доступ к символам
-str.indexOf("World")    // 7  | Поиск позиции строки
-str.includes("Hello")   // true | Существование строки
-str.slice(7, 12)        // World | Срез строки
-str.toUpperCase         // "HELLO, WORLD!"
-str.toLowerCase         // "hello, world!"
+let str = "Hello, World!";
+str.length;             // 13 | Длина строки
+str[0];                 // H  | Доступ к символам
+str.indexOf("World");   // 7  | Поиск позиции строки
+str.includes("Hello");  // true | Существование строки
+str.slice(7, 12);       // World | Срез строкип
+str.toUpperCase;        // "HELLO, WORLD!"
+str.toLowerCase;        // "hello, world!"
