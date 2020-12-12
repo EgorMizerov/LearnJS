79a1258a557c49806dd89a2a9355b6a73f07ee40
Egor Mizerov <mizerov-2003@mail.ru>
2020-12-12 16:39:51 +0500

    Edit objects.js

--- objects/objects.js
+++ objects/objects.js
@@ -54,6 +54,27 @@ function User(name = "Egor") {
     };
 }
 
+// Symbol.iterator
+let range = {
+    from: 1,
+    to: 5,
+
+    [Symbol.iterator]() {
+        return {
+            current: this.from,
+            last: this.to,
+
+            next() {
+                if (this.current <= this.last) {
+                    return {done: false, value: this.current++};
+                } else {
+                    return {done: true};
+                }
+            }
+        }
+    }
+};
+
 // Symbol.toPrimitive
 let person = {
     name: "Egor",
