2cef63aeb3bec0d0fb8d42e2d9ef0d6f5b10fb89
Egor Mizerov <mizerov-2003@mail.ru>
2020-12-13 17:46:29 +0500

    add prototype.js

--- objects/prototype.js
+++ objects/prototype.js
@@ -48,3 +48,20 @@ alert(animal.isSleeping); // undefined (нет такого свойства в 
 // Цикл for .. in
 // Цикл for..in проходит не только по собственным, но и по унаследованным свойствам объекта.
 for(let prop in rabbit) alert(prop); // jumps, затем eats
+
+/* 
+==================================================================
+Свойство __prop__ считается устаревшим и не стоит его использовать
+==================================================================
+Современные же методы это:
+Object.create(proto, [descriptors]) – создаёт пустой объект со свойством [[Prototype]], указанным как proto, и необязательными дескрипторами свойств descriptors.
+Object.getPrototypeOf(obj) – возвращает свойство [[Prototype]] объекта obj.
+Object.setPrototypeOf(obj, proto) – устанавливает свойство [[Prototype]] объекта obj как proto.
+*/
+
+// Создаём новый объект с проотипом animal
+let rabbit = Object.create(animal);
+alert(rabbit.eats); // true
+
+alert(Object.getPrototypeOf(rabbit) === animal); // получаем прототип объекта rabbit
+Object.setPrototypeOf(rabbit, {}); // заменяем прототип объекта rabbit на {}
